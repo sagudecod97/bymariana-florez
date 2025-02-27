05db3e6aed867a1e6222b5f90fdba387867aec04
Santiago A <sagudecod97@gmail.com>
2025-02-26 20:57:53 -0500

    Created about me section, bottom components left to be done

---
+++ about-me.js
@@ -0,0 +1,9 @@
+import "./index.scss";
+import "./styles/common.scss";
+import "./styles/about-me.scss";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+console.log("HMR is working!");
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: { index: "./index.js", aboutMe: "./about-me.js" },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
@@ -14,7 +14,7 @@ module.exports = {
       // SASS/SCSS Loader
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, , "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       // Image Loader
       {
@@ -38,10 +38,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html", // Source HTML
+      template: "./index.html", // Source HTML -
+    }),
+    new HtmlWebpackPlugin({
+      filename: "about-me.html",
+      template: "./about-me.html",
+      chunks: ["aboutMe"],
     }),
     new MiniCssExtractPlugin({
-      filename: "styles.css", // ✅ Output CSS file
+      filename: "[name].css", // ✅ Output CSS file
     }),
   ],
   devServer: {
